2d971ab74958123b7a2f92e7c78e44583ea52fc7
Abhishek Salgaonkar <asalgaonkar@rentpath.com>
2020-08-20 22:33:55 -0400

    Added Repository Pattern

---
+++ Taste.DataAccess/Data/Repository/Repository.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Taste.DataAccess.Data.Repository.IRepository;
+
+namespace Taste.DataAccess.Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T: class
+    {
+        protected readonly DbContext _context;
+        internal DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public T Get(int i)
+        {
+            return _dbSet.Find(i);
+        }
+
+        public IEnumerable<T> GetAll(System.Linq.Expressions.Expression<Func<T, bool>> filter = null, Func<System.Linq.IQueryable<T>, System.Linq.IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            IQueryable<T> query = _dbSet;
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if(includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' },StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            if(orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(System.Linq.Expressions.Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            {
+                IQueryable<T> query = _dbSet;
+                if (filter != null)
+                {
+                    query = query.Where(filter);
+                }
+                if (includeProperties != null)
+                {
+                    foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        query = query.Include(includeProperty);
+                    }
+                }
+                return query.FirstOrDefault();
+            }
+        }
+
+        public void Remove(int id)
+        {
+            T entityToRemove = _dbSet.Find(id);
+            Remove(entityToRemove);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
---
+++ Taste.Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Taste.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name="Category Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Category Order")]
+        public int Order { get; set; }
+
+    }
+}
