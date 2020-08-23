eac1a64abb7353901d752a2ac304e6015a3998f9
Abhishek Salgaonkar <asalgaonkar@rentpath.com>
2020-08-22 22:08:48 -0400

    Home Page UI

---
+++ Taste/Pages/Customer/Home/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
+
+namespace Taste.Pages.Customer.Home
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<MenuItem> MenuItemListItems { get; set; }
+        public IEnumerable<Category> CategoryListItems { get; set; }
+        public IActionResult OnGet()
+        {
+            MenuItemListItems = _unitOfWork.MenuItem.GetAll(null, null, "Category,FoodType");
+            CategoryListItems = _unitOfWork.Category.GetAll(null, c => c.OrderBy(c => c.Order), null);
+            return Page();
+        }
+    }
+}
--- Taste/Pages/Index.cshtml.cs
+++ Taste/Pages/Index.cshtml.cs
@@ -1,25 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace Taste.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+       
+        public IActionResult OnGet()
         {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-
+           
+            return RedirectToPage("./Customer/Home/Index");
         }
     }
 }
